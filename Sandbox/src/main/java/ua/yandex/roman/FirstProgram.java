73384bc99d8e50b4b7e67f8a54d18db3e72a64fa
Yakovlev Roman <qa-face25@yandex.ua>
2016-02-19 17:54:07 +0200

    Все конечный результат

--- Sandbox/src/main/java/ua/yandex/roman/FirstProgram.java
+++ Sandbox/src/main/java/ua/yandex/roman/FirstProgram.java
@@ -2,18 +2,17 @@ package ua.yandex.roman;
 
 public class FirstProgram {
 	public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.x = 5;
-    p1.y = 10;
 
-    Point p2 = new Point();
-    p2.x = 5;
-    p2.y = 10;
+    //Первый метод
+    System.out.println("Расстояние между двумя точками = " + new Point(5,10).distance(new Point(7,12)));
 
-    System.out.println(distance(p1,p2));
-  }
+    //Второй метод
+    System.out.println("Расстояние между двумя точками = " + new Point().distance(new Point(5,10), new Point(7,12)));
 
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(((p1.y - p1.x) * (p1.y - p1.x)) + ((p2.y - p2.x) * (p2.y - p2.x)));
+    //Третий метод такой же как второй
+    Point p1 = new Point(5,10);
+    Point p2 = new Point(7,12);
+
+    System.out.println("Расстояние между двумя точками = " + p1.distance(p1,p2));
   }
 }
--- Sandbox/src/main/java/ua/yandex/roman/Point.java
+++ Sandbox/src/main/java/ua/yandex/roman/Point.java
@@ -3,4 +3,20 @@ package ua.yandex.roman;
 public class Point {
   double x;
   double y;
+
+  public Point() {
+  }
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p) {
+    return Math.sqrt(Math.pow((p.x - this.x),2) + Math.pow((p.y - this.y),2));
+  }
+
+  public double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+  }
 }
