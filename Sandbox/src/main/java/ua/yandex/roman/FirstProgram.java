b20bedaac317a7460ea4c060ae3fbe4ea356a4dd
Yakovlev Roman <qa-face25@yandex.ua>
2016-02-19 16:41:13 +0200

    Добавлено немного функций

--- Sandbox/src/main/java/ua/yandex/roman/FirstProgram.java
+++ Sandbox/src/main/java/ua/yandex/roman/FirstProgram.java
@@ -2,6 +2,26 @@ package ua.yandex.roman;
 
 public class FirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+		hello("world!");
+    hello("user!");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("hello, " + somebody);
+  }
+
+  public static double area (double len) {
+    return len * len;
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
